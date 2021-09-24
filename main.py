7a355236a7fc76f856f74bbfd345a40b44d5cb40
Abhi kanchan <abhi.c456@gmail.com>
2021-09-24 07:54:09 +0000

    Basic python

---
+++ main.py
@@ -0,0 +1,51 @@
+age = 26;
+print("My age is",age);
+
+#comment in python
+#They are using snake case of conventions
+my_friend_age = 26;
+print("My Friend age is",my_friend_age);
+
+#If they give in Caps its constan value using in entitre project
+
+PI = 3.14159
+print("Pi value is",PI)
+
+RADIAN_TO_DEGREE = 180/PI
+print(RADIAN_TO_DEGREE)
+
+#division operation
+float_division_opearation = 12/3
+print(float_division_opearation)
+
+#(//) removes decimals only give int result
+integer_division_operation = 12//3
+print(integer_division_operation)
+print(13//2)
+
+#To find remainder
+print(12%2)
+print(14%5)
+
+a = 17.5
+b = 5
+x = a + b - 9
+print(x)
+
+#Strings
+#both are fine
+word1 = "Hello,World!"
+word2 = 'Hello,World!'
+word3 = "Hello It's me"
+word4 = 'Hi "You are amazing!"'
+print(word1,word2,word3,word4)
+
+#otherwise you have to use escape character ie \(back slash)
+word5 = 'Hello It\'s me';
+print(word5)
+
+#if you need to add multiple lines string you have to use """ (three double quotes)
+#They are using as multiple line comment also
+word6 = """hello 
+how you doing"""
+print(word6)
